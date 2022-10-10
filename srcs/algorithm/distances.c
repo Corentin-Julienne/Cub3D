66e11d829c3b87439e0ff7a82b301e873d7d2e6d
Maxime <contact@maaxit.fr>
2022-10-10 16:17:57 +0200

    Remove useless distances functions

--- srcs/algorithm/distances.c
+++ srcs/algorithm/distances.c
@@ -1,17 +1,5 @@
 #include "cub3d.h"
 
-/* Calculate the distance between 2 intersections points on X axis */
-double  calc_dist_x(double ang_y)
-{
-    return (CUBES_SIZE / tan(ang_y));
-}
-
-/* Calculate the distance between 2 intersections points on Y axis */
-double  calc_dist_y(double ang_y)
-{
-    return (CUBES_SIZE / tan(ang_y));
-}
-
 /* Calculate the distance between 2 points */
 double  calc_dist(double x1, double y1, double x2, double y2)
 {
